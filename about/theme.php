e1fd568ad442c7b08e06dc89a82c3cfdac9f19ae
Ben Keen <ben.keen@gmail.com>
2016-12-28 15:23:41 -0800

    1.0.0

---
+++ about/theme.php
@@ -0,0 +1,9 @@
+<?php
+
+$theme_name = "Oh Canada";
+$theme_author = "Encore Web Studios";
+$theme_author_email = "formtools@encorewebstudios.com";
+$theme_link = "http://themes.formtools.org";
+$theme_description = "My one and only attempt at patriotism. A red and white theme with a side helping of maple syrup and moose.";
+$theme_version = "1.0.0";
+$supports_ft_versions = "2.0.0";
